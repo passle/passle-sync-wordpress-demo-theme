454008951c43cb8c288d81093902f1d2f46abe3b
felix-packard <felix@passle.net>
2022-05-06 13:24:49 +0100

    Add noindex meta tag to the theme

--- header.php
+++ header.php
@@ -4,6 +4,7 @@
 <head>
 	<meta charset="<?php bloginfo('charset'); ?>">
 	<meta name="viewport" content="width=device-width">
+	<meta name="robots" content="noindex">
 
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
